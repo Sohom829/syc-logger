90153690245880019d77bcba367b783ee5b5f1de
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-11-12 22:02:30 +0600

    Adding 2 more unique colors

--- src/logger/utils.ts
+++ src/logger/utils.ts
@@ -7,12 +7,16 @@ const basicColors = {
   hotPink: chalk.hex("#FF69B4"),
   olive: chalk.hex("#808000"),
   cadetBlue: chalk.hex("#5F9EA0"),
+  lightCoral: chalk.hex("#f08080"),
+  seaGreen: chalk.hex("#2e8b57"),
   bgCoral: chalk.bgHex("#FF7F50"),
   bgAliceBlue: chalk.bgHex("#F0F8FF"),
   bgFireBrick: chalk.bgHex("#B22222"),
   bgHotPink: chalk.bgHex("#FF69B4"),
   bgOlive: chalk.bgHex("#808000"),
   bgCadetBlue: chalk.bgHex("#5F9EA0"),
+  bgLightCoral: chalk.bgHex("#f08080"),
+  bgSeaGreen: chalk.bgHex("#2e8b57"),
 };
 
 export { basicColors };
