a24d4a072e84d487ddf30fa9189241d765b4b67f
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-11-12 18:39:39 +0600

    Basic Color

---
+++ src/logger/utils.ts
@@ -0,0 +1,18 @@
+const chalk = require("chalk");
+
+const basicColors = {
+  coral: chalk.hex("#FF7F50"),
+  aliceBlue: chalk.hex("#F0F8FF"),
+  fireBrick: chalk.hex("#B22222"),
+  hotPink: chalk.hex("#FF69B4"),
+  olive: chalk.hex("#808000"),
+  cadetBlue: chalk.hex("#5F9EA0"),
+  bgCoral: chalk.bgHex("#FF7F50"),
+  bgAliceBlue: chalk.bgHex("#F0F8FF"),
+  bgFireBrick: chalk.bgHex("#B22222"),
+  bgHotPink: chalk.bgHex("#FF69B4"),
+  bgOlive: chalk.bgHex("#808000"),
+  bgCadetBlue: chalk.bgHex("#5F9EA0"),
+};
+
+export { basicColors };
