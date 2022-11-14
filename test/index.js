12859398f3b09f799a43d04261a12e1d5e9ef387
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-11-14 15:29:45 +0600

    Testing more thing

--- test/index.js
+++ test/index.js
@@ -4,13 +4,21 @@ const {
   logTime,
   logDate,
   logTaD,
+  logPCInfo,
+  logTaDAsync,
+  renderLinesAsync,
 } = require("../dist/index");
 
 const logColor = (message) => {
-  console.log(basicColors.bgCoral(message));
+  console.log(basicColors.seaGreen(message));
 };
 
-renderLines(40, `#808000`);
+const PCInfo = new logPCInfo();
+
+PCInfo.getPCUSerInfo();
+
+console.log(renderLinesAsync(40, "#808000"));
 logColor("Hi");
 logTaD("#808000");
+console.log(logTaDAsync("#808000"));
 renderLines(40, `#808000`);
