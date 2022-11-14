9ac980e12bf67656928ccfb6282be13c0eb8b8de
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-11-14 22:33:57 +0600

    Updated PCInfo sync and Added PCInfoAsync

--- src/logger/PCInfo.ts
+++ src/logger/PCInfo.ts
@@ -14,12 +14,21 @@ class logPCInfo {
       "getPCUptime",
       "getPCUserInfo",
       "getPCOSInfo",
+      "getPCCPUInfoAsync",
+      "getPCArchAsync",
+      "getPCFreeMemoryAsync",
+      "getPCHostNameAsync",
+      "getPCPlatformAsync",
+      "getPCReleaseAsync",
+      "getPCUptimeAsync",
+      "getPCUserInfoAsync",
+      "getPCOSInfoAsync",
     ];
   }
   async getCPUInfo(color: string = "#FFFFFF") {
     const Color = chalk.hex(`${color}`);
 
-    console.log(Color(os.cpus()));
+    console.log(Color(JSON.stringify(os.cpus())));
   }
   async getPCArch(color: string = "#FFFFFF") {
     const Color = chalk.hex(`${color}`);
@@ -48,11 +57,49 @@ class logPCInfo {
   }
   async getPCUSerInfo(color: string = "#FFFFFF") {
     const Color = chalk.hex(`${color}`);
-    console.log(Color(os.userInfo()));
+    console.log(Color(JSON.stringify(os.userInfo())));
   }
   async getPCOSInfo(color: string = "#FFFFFF") {
     const Color = chalk.hex(`${color}`);
     console.log(Color(os.type()));
+  }
+  async getCPUInfoAsync(color: string = "#FFFFFF") {
+    const Color = chalk.hex(`${color}`);
+
+    return Color(JSON.stringify(os.cpus()));
+  }
+  async getPCArchAsync(color: string = "#FFFFFF") {
+    const Color = chalk.hex(`${color}`);
+
+    return Color(os.arch());
+  }
+  async getPCFreeMemoryAsync(color: string = "#FFFFFF") {
+    const Color = chalk.hex(`${color}`);
+    return Color(os.freemem());
+  }
+  async getPCHostNameAsync(color: string = "#FFFFFF") {
+    const Color = chalk.hex(`${color}`);
+    return Color(os.hostname());
+  }
+  async getPCPlatformAsync(color: string = "#FFFFFF") {
+    const Color = chalk.hex(`${color}`);
+    return Color(os.platform());
+  }
+  async getPCReleaseAsync(color: string = "#FFFFFF") {
+    const Color = chalk.hex(`${color}`);
+    return Color(os.release());
+  }
+  async getPCUptimeAsync(color: string = "#FFFFFF") {
+    const Color = chalk.hex(`${color}`);
+    return Color(os.uptime());
+  }
+  async getPCUSerInfoAsync(color: string = "#FFFFFF") {
+    const Color = chalk.hex(`${color}`);
+    return Color(JSON.stringify(os.userInfo()));
+  }
+  async getPCOSInfoAsync(color: string = "#FFFFFF") {
+    const Color = chalk.hex(`${color}`);
+    return Color(os.type());
   }
 }
 
