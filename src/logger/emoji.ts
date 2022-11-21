9a2d3d429492ea0f8c81407b6934cf73777a5e4a
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-11-21 05:09:47 +0000

    add: logEmoji() function

---
+++ src/logger/emoji.ts
@@ -0,0 +1,36 @@
+const chalk = require("chalk");
+
+const logEmoji = (message: any, style: number = 1, color: string = "#FFFFFF", emoji: string = "✅") => {
+  const Color = chalk.hex(`${color}`);
+  if(style === 1) {
+    console.log(emoji + Color("  》" + message));
+  } else if(style === 2) {
+    console.log(emoji + Color("  〕" + message));
+  } else if(style === 3) {
+    console.log(emoji + Color("  〙" + message));
+  } else if(style === 4) {
+    console.log(emoji + Color("  ﹞ " + message));
+  }
+  else {
+    throw new TypeError(`Syc-Logger doesn\'t have ${style}th style`)
+  }
+
+}
+
+const logEmojiAsync = (message: any, style: number = 1, color: string = "#FFFFFF", emoji: string = "✅") => {
+  const Color = chalk.hex(`${color}`);
+  if(style === 1) {
+    return emoji + Color("  》" + message);
+  } else if(style === 2) {
+    return emoji + Color("  〕" + message);
+  } else if(style === 3) {
+    return emoji + Color("  〙" + message);
+  } else if(style === 4) {
+    return emoji + Color("  ﹞ " + message);
+  }
+  else {
+    return new TypeError(`Syc-Logger doesn\'t have ${style}th style`)
+  }
+}
+
+export { logEmoji, logEmojiAsync };
